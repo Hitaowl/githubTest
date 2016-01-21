19f877f964a61503b3570cae0a06cb6a3571b3f7
Hitaowl <max.richter21@gmail.com>
2016-01-21 15:58:24 +0100

    My first commit

---
+++ src/de/fhbrandenburg/hitaowl/Main.java
@@ -0,0 +1,9 @@
+package de.fhbrandenburg.hitaowl;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+    }
+}
